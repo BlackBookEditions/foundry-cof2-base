8f8a7e2c5f16c0fcc19061216c79fd980b8775d2
Christophe Laudon <ch.laudon@gmail.com>
2025-09-27 18:10:05 +0200

    Ajout du menu

--- cof-base.mjs
+++ cof-base.mjs
@@ -1,150 +1,150 @@
 Hooks.once("init", () => {
-  console.info("COF Livre de base | Début de l'initialisation du module...")
+  console.info("COF Livre des règles | Initialisation du module...")
 
   // Load Martial Training
   if (game.system.CONST.martialTrainingsWeapons.length == 0) {
     // Mains nues
-    game.system.CONST.martialTrainingsWeapons.push({ key: "unarmed", label: "COF.config.martialTrainingWeapon.unarmed" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "unarmed", label: "COFBASE.config.martialTrainingWeapon.unarmed" })
 
     // Bâton
-    game.system.CONST.martialTrainingsWeapons.push({ key: "staff", label: "COF.config.martialTrainingWeapon.staff" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "staff", label: "COFBASE.config.martialTrainingWeapon.staff" })
 
     // Bâton ferré
-    game.system.CONST.martialTrainingsWeapons.push({ key: "ironStaff", label: "COF.config.martialTrainingWeapon.ironStaff" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "ironStaff", label: "COFBASE.config.martialTrainingWeapon.ironStaff" })
 
     // Dague
-    game.system.CONST.martialTrainingsWeapons.push({ key: "dagger", label: "COF.config.martialTrainingWeapon.dagger" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "dagger", label: "COFBASE.config.martialTrainingWeapon.dagger" })
 
     // Épée à deux mains
-    game.system.CONST.martialTrainingsWeapons.push({ key: "greatSword", label: "COF.config.martialTrainingWeapon.greatSword" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "greatSword", label: "COFBASE.config.martialTrainingWeapon.greatSword" })
 
     // Épée bâtarde
-    game.system.CONST.martialTrainingsWeapons.push({ key: "bastardSword", label: "COF.config.martialTrainingWeapon.bastardSword" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "bastardSword", label: "COFBASE.config.martialTrainingWeapon.bastardSword" })
 
     // Épée courte
-    game.system.CONST.martialTrainingsWeapons.push({ key: "shortSword", label: "COF.config.martialTrainingWeapon.shortSword" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "shortSword", label: "COFBASE.config.martialTrainingWeapon.shortSword" })
 
     // Épée longue
-    game.system.CONST.martialTrainingsWeapons.push({ key: "longSword", label: "COF.config.martialTrainingWeapon.longSword" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "longSword", label: "COFBASE.config.martialTrainingWeapon.longSword" })
 
     // Épieu
-    game.system.CONST.martialTrainingsWeapons.push({ key: "spear", label: "COF.config.martialTrainingWeapon.spear" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "spear", label: "COFBASE.config.martialTrainingWeapon.spear" })
 
     // Fléau
-    game.system.CONST.martialTrainingsWeapons.push({ key: "flail", label: "COF.config.martialTrainingWeapon.flail" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "flail", label: "COFBASE.config.martialTrainingWeapon.flail" })
 
     // Fléau à deux mains
-    game.system.CONST.martialTrainingsWeapons.push({ key: "greatFlail", label: "COF.config.martialTrainingWeapon.greatFlail" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "greatFlail", label: "COFBASE.config.martialTrainingWeapon.greatFlail" })
 
     // Gourdin
-    game.system.CONST.martialTrainingsWeapons.push({ key: "club", label: "COF.config.martialTrainingWeapon.club" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "club", label: "COFBASE.config.martialTrainingWeapon.club" })
 
     // Hache
-    game.system.CONST.martialTrainingsWeapons.push({ key: "axe", label: "COF.config.martialTrainingWeapon.axe" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "axe", label: "COFBASE.config.martialTrainingWeapon.axe" })
 
     // Hache à deux mains
-    game.system.CONST.martialTrainingsWeapons.push({ key: "greatAxe", label: "COF.config.martialTrainingWeapon.greatAxe" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "greatAxe", label: "COFBASE.config.martialTrainingWeapon.greatAxe" })
 
     // Lance
-    game.system.CONST.martialTrainingsWeapons.push({ key: "lance", label: "COF.config.martialTrainingWeapon.lance" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "lance", label: "COFBASE.config.martialTrainingWeapon.lance" })
 
     // Lance de cavalerie
-    game.system.CONST.martialTrainingsWeapons.push({ key: "cavalryLance", label: "COF.config.martialTrainingWeapon.cavalryLance" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "cavalryLance", label: "COFBASE.config.martialTrainingWeapon.cavalryLance" })
 
     // Marteau
-    game.system.CONST.martialTrainingsWeapons.push({ key: "hammer", label: "COF.config.martialTrainingWeapon.hammer" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "hammer", label: "COFBASE.config.martialTrainingWeapon.hammer" })
 
     // Masse
-    game.system.CONST.martialTrainingsWeapons.push({ key: "mace", label: "COF.config.martialTrainingWeapon.mace" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "mace", label: "COFBASE.config.martialTrainingWeapon.mace" })
 
     // Pique
-    game.system.CONST.martialTrainingsWeapons.push({ key: "pike", label: "COF.config.martialTrainingWeapon.pike" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "pike", label: "COFBASE.config.martialTrainingWeapon.pike" })
 
     // Rapière
-    game.system.CONST.martialTrainingsWeapons.push({ key: "rapier", label: "COF.config.martialTrainingWeapon.rapier" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "rapier", label: "COFBASE.config.martialTrainingWeapon.rapier" })
 
     // Stylet
-    game.system.CONST.martialTrainingsWeapons.push({ key: "stiletto", label: "COF.config.martialTrainingWeapon.stiletto" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "stiletto", label: "COFBASE.config.martialTrainingWeapon.stiletto" })
 
     // Vivelame
-    game.system.CONST.martialTrainingsWeapons.push({ key: "swiftblade", label: "COF.config.martialTrainingWeapon.swiftblade" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "swiftblade", label: "COFBASE.config.martialTrainingWeapon.swiftblade" })
 
     // Arbalète de poing
-    game.system.CONST.martialTrainingsWeapons.push({ key: "handCrossbow", label: "COF.config.martialTrainingWeapon.handCrossbow" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "handCrossbow", label: "COFBASE.config.martialTrainingWeapon.handCrossbow" })
 
     // Arbalète légère
-    game.system.CONST.martialTrainingsWeapons.push({ key: "lightCrossbow", label: "COF.config.martialTrainingWeapon.lightCrossbow" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "lightCrossbow", label: "COFBASE.config.martialTrainingWeapon.lightCrossbow" })
 
     // Arbalète lourde
-    game.system.CONST.martialTrainingsWeapons.push({ key: "heavyCrossbow", label: "COF.config.martialTrainingWeapon.heavyCrossbow" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "heavyCrossbow", label: "COFBASE.config.martialTrainingWeapon.heavyCrossbow" })
 
     // Arc court
-    game.system.CONST.martialTrainingsWeapons.push({ key: "shortBow", label: "COF.config.martialTrainingWeapon.shortBow" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "shortBow", label: "COFBASE.config.martialTrainingWeapon.shortBow" })
 
     // Arc long
-    game.system.CONST.martialTrainingsWeapons.push({ key: "longBow", label: "COF.config.martialTrainingWeapon.longBow" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "longBow", label: "COFBASE.config.martialTrainingWeapon.longBow" })
 
     // Couteau de lancer
-    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingKnife", label: "COF.config.martialTrainingWeapon.throwingKnife" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingKnife", label: "COFBASE.config.martialTrainingWeapon.throwingKnife" })
 
     // Dague (version jet)
-    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingDagger", label: "COF.config.martialTrainingWeapon.throwingDagger" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingDagger", label: "COFBASE.config.martialTrainingWeapon.throwingDagger" })
 
     // Fronde
-    game.system.CONST.martialTrainingsWeapons.push({ key: "sling", label: "COF.config.martialTrainingWeapon.sling" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "sling", label: "COFBASE.config.martialTrainingWeapon.sling" })
 
     // Hachette (version jet)
-    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingAxe", label: "COF.config.martialTrainingWeapon.throwingAxe" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingAxe", label: "COFBASE.config.martialTrainingWeapon.throwingAxe" })
 
     // Javelot
-    game.system.CONST.martialTrainingsWeapons.push({ key: "javelin", label: "COF.config.martialTrainingWeapon.javelin" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "javelin", label: "COFBASE.config.martialTrainingWeapon.javelin" })
 
     // Lance (version jet)
-    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingSpear", label: "COF.config.martialTrainingWeapon.throwingSpear" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "throwingSpear", label: "COFBASE.config.martialTrainingWeapon.throwingSpear" })
 
     // Pétoire
-    game.system.CONST.martialTrainingsWeapons.push({ key: "handCannon", label: "COF.config.martialTrainingWeapon.handCannon" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "handCannon", label: "COFBASE.config.martialTrainingWeapon.handCannon" })
 
     // Mousquet
-    game.system.CONST.martialTrainingsWeapons.push({ key: "musket", label: "COF.config.martialTrainingWeapon.musket" })
+    game.system.CONST.martialTrainingsWeapons.push({ key: "musket", label: "COFBASE.config.martialTrainingWeapon.musket" })
   }
 
   if (game.system.CONST.martialTrainingsArmors.length == 0) {
     // Tissus matelassés, fourrures
-    game.system.CONST.martialTrainingsArmors.push({ key: "paddedClothFur", label: "COF.config.martialTrainingArmor.paddedClothFur" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "paddedClothFur", label: "COFBASE.config.martialTrainingArmor.paddedClothFur" })
 
     // Cuir simple
-    game.system.CONST.martialTrainingsArmors.push({ key: "leather", label: "COF.config.martialTrainingArmor.leather" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "leather", label: "COFBASE.config.martialTrainingArmor.leather" })
 
     // Cuir renforcé, broigne
-    game.system.CONST.martialTrainingsArmors.push({ key: "studdedLeather", label: "COF.config.martialTrainingArmor.studdedLeather" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "studdedLeather", label: "COFBASE.config.martialTrainingArmor.studdedLeather" })
 
     // Chemise de mailles
-    game.system.CONST.martialTrainingsArmors.push({ key: "chainShirt", label: "COF.config.martialTrainingArmor.chainShirt" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "chainShirt", label: "COFBASE.config.martialTrainingArmor.chainShirt" })
 
     // Cotte de mailles
-    game.system.CONST.martialTrainingsArmors.push({ key: "chainMail", label: "COF.config.martialTrainingArmor.chainMail" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "chainMail", label: "COFBASE.config.martialTrainingArmor.chainMail" })
 
     // Armure de plaques
-    game.system.CONST.martialTrainingsArmors.push({ key: "plateArmor", label: "COF.config.martialTrainingArmor.plateArmor" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "plateArmor", label: "COFBASE.config.martialTrainingArmor.plateArmor" })
 
     // Plaque complète
-    game.system.CONST.martialTrainingsArmors.push({ key: "fullPlate", label: "COF.config.martialTrainingArmor.fullPlate" })
+    game.system.CONST.martialTrainingsArmors.push({ key: "fullPlate", label: "COFBASE.config.martialTrainingArmor.fullPlate" })
   }
 
   if (game.system.CONST.martialTrainingsShields.length == 0) {
     // Petit bouclier
-    game.system.CONST.martialTrainingsShields.push({ key: "smallShield", label: "COF.config.martialTrainingShield.smallShield" })
+    game.system.CONST.martialTrainingsShields.push({ key: "smallShield", label: "COFBASE.config.martialTrainingShield.smallShield" })
 
     // Grand bouclier
-    game.system.CONST.martialTrainingsShields.push({ key: "largeShield", label: "COF.config.martialTrainingShield.largeShield" })
+    game.system.CONST.martialTrainingsShields.push({ key: "largeShield", label: "COFBASE.config.martialTrainingShield.largeShield" })
   }
 
   console.info("COF Livre de base | Fin de l'initialisation du module")
 })
 
 Hooks.once("ready", async () => {
-  console.info("COF Livre de base | Module prêt")
+  console.info("COF Livre des règles | Module prêt")
 })
 
 /*
@@ -153,5 +153,24 @@ Hooks.once("ready", async () => {
 Hooks.on("renderJournalEntrySheet", (application, element, context, options) => {
   if (application.document.getFlag("cof-base", "isJournalCOF") === true) {
     element.classList.add("journal-cof-base")
+  }
+})
+
+/*
+ * Hook renderCOSidebarMenu
+ */
+Hooks.on("renderCOSidebarMenu", async (application, html, context, options) => {
+  console.log("COF Livre des règles | Ajout du menu dans la barre latérale")
+  let element = html.querySelector(".co.support")
+  //if (!element) element = html.querySelector(".co.system")
+
+  if (element) {
+      const renderedHtml = await foundry.applications.handlebars.renderTemplate("modules/cof-base/templates/sidebar-menu.hbs", {
+      user: game.user,
+    })
+
+    if (renderedHtml !== "") {
+      element.insertAdjacentHTML("afterend", renderedHtml)
+    }
   }
 })
